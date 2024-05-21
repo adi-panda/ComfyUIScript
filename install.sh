860588ed0ef8f343d5c7761cf65463dec071fe92
Your Name <you@example.com>
2024-05-21 21:07:30 +0000

    dada

--- install.sh
+++ install.sh
@@ -25,16 +25,21 @@ wget --content-disposition -P models/clip_vision "$clip_vision_url"
 
 # Create the directory if it doesn't exist
 mkdir -p models/inpaint
-inpaint_head="https://huggingface.co/lllyasviel/fooocus_inpaint/blob/main/fooocus_inpaint_head.pth"
-inpaint_patch="https://huggingface.co/lllyasviel/fooocus_inpaint/blob/main/inpaint_v26.fooocus.patch"
+inpaint_head="https://huggingface.co/lllyasviel/fooocus_inpaint/resolve/main/fooocus_inpaint_head.pth"
+inpaint_patch="https://huggingface.co/lllyasviel/fooocus_inpaint/resolve/main/inpaint_v26.fooocus.patch"
 big_lama="https://github.com/Sanster/models/releases/download/add_big_lama/big-lama.pt"
 mat="https://github.com/Sanster/models/releases/download/add_mat/Places_512_FullData_G.pth"
 
-wget --content-disposition -P models/inpaint/fooocus_inpaint_head.pth "$inpaint_head"
-wget --content-disposition -P models/inpaint/inpaint_v26.fooocus.patch "$inpaint_patch"
-wget --content-disposition -P models/inpaint/big-lama.pt "$big_lama"
-wget --content-disposition -P models/inpaint/Places_512_FullData_G.pth "$mat"
+wget --content-disposition -P models/inpaint "$inpaint_head"
+wget --content-disposition -P models/inpaint "$inpaint_patch"
+wget --content-disposition -P models/inpaint "$big_lama"
+wget --content-disposition -P models/inpaint "$mat"
 
 pip install -r requirements.txt
 
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git custom_nodes/ComfyUI_Manager
+git clone https://github.com/WASasquatch/was-node-suite-comfyui custom_nodes/was-node-suite-comfyui
+git clone https://github.com/BadCafeCode/masquerade-nodes-comfyui custom_nodes/masquerade-nodes-comfyui
+git clone https://github.com/JPS-GER/ComfyUI_JPS-Nodes custom_nodes/ComfyUI_JPS-Nodes
+git clone https://github.com/Acly/comfyui-inpaint-nodes custom_nodes/comfyui-inpaint-nodes
+
